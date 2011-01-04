8557a45564ddbc39794704a904c6e1e233f08f3f
Hans Fraiponts <hansf@GITS.(none)>
2011-01-04 15:03:19 +0100

    first files

---
+++ empty.php
@@ -0,0 +1,17 @@
+<?
+/*
+
+	geld in kas : 
+	SELECT sum(`amount`) FROM `transactions` WHERE `amount` > 0 AND `date` > '2010-01-02 17:34:56'
+
+	Laatste ophaling: 
+	SELECT time FROM `withdraw` WHERE 1 order by id desc LIMIT  0,1
+
+	update:
+	$sql = "INSERT INTO `shop`.`withdraw` (`id`, `time`) VALUES (NULL, CURRENT_TIMESTAMP);";
+
+	*/
+
+
+
+?> 
