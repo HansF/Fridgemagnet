626501d8d99792319e0bfddaa9d0583efea5583f
Hans Fraiponts <fraiponts@gmail.com>
2011-01-09 17:19:23 +0100

    Empty kassa prelimirary

--- extra/dbschema.sql
+++ extra/dbschema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 02, 2011 at 06:14 PM
+-- Generation Time: Jan 09, 2011 at 05:14 
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -16,7 +16,29 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `deposit`a
+-- Table structure for table `actioncodes`
+--
+
+CREATE TABLE IF NOT EXISTS `actioncodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action` varchar(250) NOT NULL,
+  `ean` varchar(13) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `actioncodes`
+--
+
+INSERT INTO `actioncodes` (`id`, `action`, `ean`) VALUES
+(1, 'delete', '1000'),
+(2, 'deposit', '1001'),
+(3, 'empty', '1002');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deposit`
 --
 
 CREATE TABLE IF NOT EXISTS `deposit` (
@@ -25,12 +47,29 @@ CREATE TABLE IF NOT EXISTS `deposit` (
   `checked_by` varchar(40) NOT NULL,
   `transaction_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `deposit`
 --
 
+INSERT INTO `deposit` (`id`, `deposit_by`, `checked_by`, `transaction_id`) VALUES
+(1, '2', '3', 4),
+(2, '1', '2', 7),
+(3, '1', '2', 8),
+(4, '1', '2', 9),
+(5, '1', '2', 10),
+(6, '1', '2', 11),
+(7, '1', '2', 12),
+(8, '1', '2', 13),
+(9, '', '2', 14),
+(10, '', '2', 15),
+(11, '1', '2', 31),
+(12, '1', '2', 33),
+(13, '1', '2', 34),
+(14, '1', '2', 37),
+(15, '1', '2', 39),
+(16, '1', '2', 40);
 
 -- --------------------------------------------------------
 
@@ -68,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `user` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
 
 --
 -- Dumping data for table `transactions`
@@ -79,7 +118,43 @@ INSERT INTO `transactions` (`id`, `amount`, `user`, `date`) VALUES
 (2, -1, 1, '2011-01-02 17:09:08'),
 (3, -1.5, 1, '2011-01-02 17:09:08'),
 (4, 5, 2, '2011-01-02 17:12:01'),
-(5, -1, 2, '2011-01-02 17:12:01');
+(5, -1, 2, '2011-01-02 17:12:01'),
+(6, 2, 3, '2011-01-05 20:16:07'),
+(7, 10, 1, '2011-01-05 20:26:31'),
+(8, 23, 1, '2011-01-05 21:00:04'),
+(9, 23, 1, '2011-01-05 21:47:28'),
+(10, 10, 1, '2011-01-05 21:55:06'),
+(11, 10, 1, '2011-01-05 22:06:47'),
+(12, 10, 1, '2011-01-05 22:16:18'),
+(13, 10, 1, '2011-01-05 22:19:31'),
+(14, 10, 0, '2011-01-05 22:20:17'),
+(15, 10, 0, '2011-01-05 22:21:17'),
+(16, 1, 1, '2011-01-05 22:33:23'),
+(17, 7.5, 1, '2011-01-05 22:47:59'),
+(18, 7, 1, '2011-01-05 22:48:49'),
+(19, 9.5, -1, '2011-01-05 22:50:18'),
+(20, -11, 1, '2011-01-05 22:51:10'),
+(21, -1.5, 1, '2011-01-05 22:51:40'),
+(22, -1.5, 1, '2011-01-05 22:59:06'),
+(23, -1.5, 2, '2011-01-05 22:59:21'),
+(24, -10, 1, '2011-01-05 23:00:49'),
+(25, -1.5, 1, '2011-01-05 23:01:46'),
+(26, -1.5, 1, '2011-01-05 23:04:28'),
+(27, -6, 1, '2011-01-05 23:29:01'),
+(28, -1.5, 1, '2011-01-05 23:30:01'),
+(29, -1.5, 1, '2011-01-05 23:31:36'),
+(30, -3, 1, '2011-01-06 17:15:27'),
+(31, 10, 1, '2011-01-06 17:15:52'),
+(32, -3, 1, '2011-01-06 17:22:58'),
+(33, 10, 1, '2011-01-06 17:23:41'),
+(34, 10, 1, '2011-01-06 17:24:21'),
+(35, -3, 1, '2011-01-06 17:24:54'),
+(36, -4, 1, '2011-01-06 21:05:26'),
+(37, 10, 1, '2011-01-06 21:05:46'),
+(38, -3, 1, '2011-01-06 22:00:14'),
+(39, 10, 1, '2011-01-06 22:01:39'),
+(40, 10, 1, '2011-01-09 14:30:47'),
+(41, -3, 1, '2011-01-09 14:31:15');
 
 -- --------------------------------------------------------
 
@@ -99,8 +174,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `ean`) VALUES
-(1, 'Hans F', '3333'),
-(2, 'Sandb', '232455');
+(1, 'Hans F', '4242'),
+(2, 'Sandb', '4343');
 
 -- --------------------------------------------------------
 
