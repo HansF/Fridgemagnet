cae2b2391abf45c6ad6ec923d552d5b332ca84b9
Hans Fraiponts <fraiponts@gmail.com>
2011-01-17 03:42:02 +0100

    Users can't sef validate is now a setting

--- inc/settings.php
+++ inc/settings.php
@@ -7,9 +7,7 @@ $dbpasswd ="";
 $cashsign = "&euro;";
 
 // application settings 
-$deletecode = 	"1000";
-$admincode = 	"9997990941687";
-$SomeSecret = "r]JbwReTH1pCzW0n9c(j2Wtjx9cM(SfvKAt-Vy}-";
+$usercantselfvalidate = true; 
 
 
 ?>
