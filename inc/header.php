23be9b023cba5de7b840e65b8b987998a082019b
Hans Fraiponts <fraiponts@gmail.com>
2011-01-26 09:04:36 +0100

    Wording

--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>0x20 Kassa</title>
+<title>0x20 Cash Register</title>
 <link rel="stylesheet" type="text/css" href="./css/960.css" />
 <script type="text/javascript" src="./js/jquery-1.4.4.min.js"></script>
 
-// <script type="text/javascript">
+<script type="text/javascript">
     $(document).ready(function(){
         //  Focus auto-focus fields
         $('.auto-focus').focus();
@@ -21,11 +21,8 @@
 
 <div id="Topbar" class="grid_12 ">
 
-<h1>- Kassa - </h1>
-<?php
-// print_R($_SESSION);
-// print_R($_POST);
-?>
+<h1>FridgeMagnet</h1>
+
 </div>
 
 </div>
