98c163cffc94d0c2ac4530834be779f23135c087
Hans Fraiponts <fraiponts@gmail.com>
2011-01-27 20:36:31 +0100

    more data

--- extra/shop.sql
+++ extra/shop.sql
@@ -3,17 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 27, 2011 at 08:24 
+-- Generation Time: Jan 17, 2011 at 02:43 
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `shop`
@@ -29,8 +23,7 @@ CREATE TABLE IF NOT EXISTS `actioncodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `action` varchar(250) NOT NULL,
   `ean` varchar(13) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ean` (`ean`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
@@ -38,9 +31,9 @@ CREATE TABLE IF NOT EXISTS `actioncodes` (
 --
 
 INSERT INTO `actioncodes` (`id`, `action`, `ean`) VALUES
-(1, 'delete', '000000012345'),
-(2, 'deposit', '000000012346'),
-(3, 'empty', '1002');
+(1, 'delete', '0000000123457'),
+(2, 'deposit', '0000000123464'),
+(3, 'empty', '2767370726258');
 
 -- --------------------------------------------------------
 
@@ -69,22 +62,22 @@ CREATE TABLE IF NOT EXISTS `deposit` (
 
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
   `price` float NOT NULL,
   `ean` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ean` (`ean`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `ean`) VALUES
-(5, 'Club-Mate', 1.5, '1111'),
+(5, 'Club-Mate', 1.5, '4029764001807'),
 (6, 'Duvel', 1.5, '2222'),
 (7, 'Bier', 1, '3333'),
-(8, 'Cola-light', 1, '4444');
+(8, 'Cola-light', 1, '4444'),
+(9, 'Hausmarke Cola', 1.5, '1094922319639');
 
 -- --------------------------------------------------------
 
@@ -115,8 +108,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(75) NOT NULL,
   `ean` varchar(40) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ean` (`ean`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
@@ -124,8 +116,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `ean`) VALUES
-(1, 'Hans F', '4242'),
-(2, 'Sandb', '4343');
+(1, 'Hans F', '5969661663175'),
+(2, 'Sandb', '5483432357372');
 
 -- --------------------------------------------------------
 
@@ -137,15 +129,11 @@ CREATE TABLE IF NOT EXISTS `withdraw` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `withdraw`
 --
 
 INSERT INTO `withdraw` (`id`, `time`) VALUES
-(1, '2011-01-17 03:35:08');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(5, '2011-01-17 14:40:54');
